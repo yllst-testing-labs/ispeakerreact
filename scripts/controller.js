4ede1020c7af4187526e46fbb4c3632f8c1fbd0f
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2023-07-21 22:08:59 +0700

    Fix the info button style

--- scripts/controller.js
+++ scripts/controller.js
@@ -1549,7 +1549,7 @@ angular.module('myApp.controllers', ['ngAnimate']).controller('myCtrl', function
 
             $($event.currentTarget).parents('tbody').find('tr[data-notesText]').fadeOut(300).remove();
             $($event.currentTarget).parents('tbody').find('.notesText').fadeOut(300);
-            var txt = '<tr data-notesText><td class="p-0" colspan="' + cols + '"><div class="notesText p-2 d-block">' + info + '</td></div></tr>';
+            var txt = '<tr data-notesText><td class="p-0" colspan="' + cols + '"><div class="notesText text-primary-emphasis bg-primary-subtle p-2 d-block">' + info + '</td></div></tr>';
             $($event.currentTarget).parents('tr').after(txt);
         } else {
             $($event.currentTarget).removeClass('selected');

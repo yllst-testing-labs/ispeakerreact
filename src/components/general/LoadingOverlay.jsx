5a3a1e3618b760b3ae0d87432c82bdc774ed8260
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-08-26 13:07:05 +0700

    Update LoadingOverlay.jsx

--- src/components/general/LoadingOverlay.jsx
+++ src/components/general/LoadingOverlay.jsx
@@ -17,7 +17,7 @@ const LoadingOverlay = () => {
                 justifyContent: "center",
                 alignItems: "center",
             }}>
-            <Spinner animation="border" role="status" variant="primary" style={{width: "4rem", height: "4rem"}}>
+            <Spinner animation="border" role="status" variant="primary" style={{ width: "4rem", height: "4rem" }}>
                 <span className="visually-hidden">Loading...</span>
             </Spinner>
         </div>

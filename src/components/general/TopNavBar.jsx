cef8ad2db0abec6512df69ace416af9a11868f5d
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-08-27 18:26:24 +0700

    Update TopNavBar.jsx

--- src/components/general/TopNavBar.jsx
+++ src/components/general/TopNavBar.jsx
@@ -6,18 +6,37 @@ const TopNavBar = () => {
         <div className="mb-4">
             <Navbar expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand as={NavLink} to="/">
-                        <img alt="" src="/images/icons/ios/32.png" className="d-inline-block align-top" /> Oxford iSpeaker
+                    <Navbar.Brand as={NavLink} to="/" className="fw-semibold">
+                        <img
+                            alt=""
+                            src="/images/icons/ios/128.png"
+                            width="32"
+                            height="32"
+                            className="d-inline-block align-top"
+                        />{" "}
+                        Oxford iSpeaker
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="top-NavBar" />
                     <Navbar.Collapse id="top-NavBar">
                         <Nav className="me-auto" variant="underline">
-                            <NavLink to="/" className="nav-link">Home</NavLink>
-                            <NavLink to="/sounds" className="nav-link">Sounds</NavLink>
-                            <Nav.Link href="#" disabled>Exercises</Nav.Link>
-                            <NavLink to="/conversations" className="nav-link">Conversations</NavLink>
-                            <Nav.Link href="#" disabled>Exams</Nav.Link>
-                            <NavLink to="/help" className="nav-link">Help</NavLink>
+                            <NavLink to="/" className="nav-link">
+                                Home
+                            </NavLink>
+                            <NavLink to="/sounds" className="nav-link">
+                                Sounds
+                            </NavLink>
+                            <Nav.Link href="#" disabled>
+                                Exercises
+                            </Nav.Link>
+                            <NavLink to="/conversations" className="nav-link">
+                                Conversations
+                            </NavLink>
+                            <Nav.Link href="#" disabled>
+                                Exams
+                            </Nav.Link>
+                            <NavLink to="/help" className="nav-link">
+                                Help
+                            </NavLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>

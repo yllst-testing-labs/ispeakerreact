e39514d8dea56fc561d5dc94868dedbd4bbec449
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-08-29 09:34:18 +0700

    Refactor and optimize the Sounds section

---
+++ src/components/sound_page/ReviewCard.jsx
@@ -0,0 +1,31 @@
+import he from "he";
+import { Card, Col, Row } from "react-bootstrap";
+import { EmojiSmile, EmojiNeutral, EmojiFrown } from "react-bootstrap-icons";
+
+const ReviewCard = ({ sound, handleReviewClick, emojiStyle }) => {
+    return (
+        <Card>
+            <Card.Header className="fw-semibold">Review</Card.Header>
+            <Card.Body>
+                <Card.Text>How do you pronounce the sound {he.decode(sound.phoneme)}?</Card.Text>
+                <Row className="d-flex justify-content-center">
+                    <Col xs={"auto"} className="me-2" onClick={() => handleReviewClick("good")}>
+                        <EmojiSmile size={52} role="button" className={`bi bi-emoji-smile ${emojiStyle("good")}`} />
+                    </Col>
+                    <Col xs={"auto"} className="me-2" onClick={() => handleReviewClick("neutral")}>
+                        <EmojiNeutral
+                            size={52}
+                            role="button"
+                            className={`bi bi-emoji-neutral ${emojiStyle("neutral")}`}
+                        />
+                    </Col>
+                    <Col xs={"auto"} className="me-2" onClick={() => handleReviewClick("bad")}>
+                        <EmojiFrown size={52} role="button" className={`bi bi-emoji-frown ${emojiStyle("bad")}`} />
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default ReviewCard;
---
+++ src/components/sound_page/SoundPracticeCard.jsx
@@ -0,0 +1,142 @@
+import { Card, Col, Row } from "react-bootstrap";
+import he from "he";
+import { RecordCircleFill, PlayCircleFill } from "react-bootstrap-icons";
+
+const SoundCardItem = ({
+    id,
+    imgPhonemeThumbSrc,
+    handleShow,
+    textContent,
+    activeRecordingCard,
+    isRecordingPlaying,
+    activePlaybackCard,
+    isRecordingPlayingActive,
+    isRecordingAvailable,
+    handleRecording,
+    handlePlayRecording,
+}) => (
+    <Card className="mb-2">
+        <Card.Body>
+            <Row>
+                <Col xs={"auto"} className="d-flex align-items-center">
+                    <button type="button" className="btn focus-ring p-1" onClick={() => handleShow(id)}>
+                        <img src={imgPhonemeThumbSrc} alt="Phoneme Thumbnail" />
+                    </button>
+                </Col>
+                <Col xs={"auto"} className="d-flex align-items-center">
+                    <span dangerouslySetInnerHTML={{ __html: he.decode(textContent) }}></span>
+                </Col>
+                <Col xs={"auto"} className="d-flex align-items-center">
+                    <RecordCircleFill
+                        size={24}
+                        role="button"
+                        className={`bi me-2 ${
+                            activeRecordingCard === id
+                                ? "text-danger"
+                                : activeRecordingCard !== null || isRecordingPlaying
+                                ? "pe-none opacity-25"
+                                : ""
+                        }`}
+                        onClick={() => handleRecording(id)}
+                    />
+                    <PlayCircleFill
+                        size={24}
+                        role="button"
+                        className={`bi me-2 ${
+                            isRecordingPlaying && activePlaybackCard !== id
+                                ? "pe-none opacity-25"
+                                : isRecordingPlayingActive(id)
+                                ? "text-success"
+                                : !isRecordingAvailable(id) || activeRecordingCard !== null
+                                ? "pe-none opacity-25"
+                                : ""
+                        }`}
+                        onClick={() => (isRecordingAvailable(id) ? handlePlayRecording(id) : null)}
+                        disabled={isRecordingPlaying && activePlaybackCard !== id}
+                    />
+                </Col>
+            </Row>
+        </Card.Body>
+    </Card>
+);
+
+const SoundPracticeCard = ({
+    sound,
+    handleShow,
+    imgPhonemeThumbSrc,
+    activeRecordingCard,
+    isRecordingPlaying,
+    activePlaybackCard,
+    accentData,
+    isRecordingPlayingActive,
+    isRecordingAvailable,
+    handleRecording,
+    handlePlayRecording,
+}) => {
+    return (
+        <>
+            {sound.shouldShow !== false && (
+                <SoundCardItem
+                    id={1}
+                    imgPhonemeThumbSrc={imgPhonemeThumbSrc}
+                    handleShow={handleShow}
+                    textContent={sound.phoneme}
+                    activeRecordingCard={activeRecordingCard}
+                    isRecordingPlaying={isRecordingPlaying}
+                    activePlaybackCard={activePlaybackCard}
+                    isRecordingPlayingActive={isRecordingPlayingActive}
+                    isRecordingAvailable={isRecordingAvailable}
+                    handleRecording={handleRecording}
+                    handlePlayRecording={handlePlayRecording}
+                />
+            )}
+            {accentData.initial && (
+                <SoundCardItem
+                    id={2}
+                    imgPhonemeThumbSrc={imgPhonemeThumbSrc}
+                    handleShow={handleShow}
+                    textContent={accentData.initial}
+                    activeRecordingCard={activeRecordingCard}
+                    isRecordingPlaying={isRecordingPlaying}
+                    activePlaybackCard={activePlaybackCard}
+                    isRecordingPlayingActive={isRecordingPlayingActive}
+                    isRecordingAvailable={isRecordingAvailable}
+                    handleRecording={handleRecording}
+                    handlePlayRecording={handlePlayRecording}
+                />
+            )}
+            {accentData.medial && (
+                <SoundCardItem
+                    id={3}
+                    imgPhonemeThumbSrc={imgPhonemeThumbSrc}
+                    handleShow={handleShow}
+                    textContent={accentData.medial}
+                    activeRecordingCard={activeRecordingCard}
+                    isRecordingPlaying={isRecordingPlaying}
+                    activePlaybackCard={activePlaybackCard}
+                    isRecordingPlayingActive={isRecordingPlayingActive}
+                    isRecordingAvailable={isRecordingAvailable}
+                    handleRecording={handleRecording}
+                    handlePlayRecording={handlePlayRecording}
+                />
+            )}
+            {accentData.final && (
+                <SoundCardItem
+                    id={4}
+                    imgPhonemeThumbSrc={imgPhonemeThumbSrc}
+                    handleShow={handleShow}
+                    textContent={accentData.final}
+                    activeRecordingCard={activeRecordingCard}
+                    isRecordingPlaying={isRecordingPlaying}
+                    activePlaybackCard={activePlaybackCard}
+                    isRecordingPlayingActive={isRecordingPlayingActive}
+                    isRecordingAvailable={isRecordingAvailable}
+                    handleRecording={handleRecording}
+                    handlePlayRecording={handlePlayRecording}
+                />
+            )}
+        </>
+    );
+};
+
+export default SoundPracticeCard;

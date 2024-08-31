0ddaa3bc785f8e7f9f00055ec72aaba7892bbeca
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-08-31 16:57:33 +0700

    Visual tweaks to the navigation bar

--- src/components/general/TopNavBar.jsx
+++ src/components/general/TopNavBar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { Nav, Navbar, Container } from "react-bootstrap";
+import { House, Mic, CardChecklist, ChatDots, ClipboardCheck } from "react-bootstrap-icons";
 
 const TopNavBar = () => {
     return (
@@ -8,7 +9,7 @@ const TopNavBar = () => {
                 <Container>
                     <Navbar.Brand as={NavLink} to="/" className="fw-semibold">
                         <img
-                            alt=""
+                            alt="Logo"
                             src="/images/icons/ios/128.png"
                             width="32"
                             height="32"
@@ -17,21 +18,26 @@ const TopNavBar = () => {
                         Oxford iSpeaker
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="top-NavBar" />
-                    <Navbar.Collapse id="top-NavBar">
+                    <Navbar.Collapse id="top-NavBar" className="mt-3 mt-md-0">
                         <Nav className="me-auto" variant="underline">
                             <NavLink to="/" className="nav-link">
+                                <House className="me-2" />
                                 Home
                             </NavLink>
                             <NavLink to="/sounds" className="nav-link">
+                                <Mic className="me-2" />
                                 Sounds
                             </NavLink>
                             <NavLink to="/exercises" className="nav-link">
+                                <CardChecklist className="me-2" />
                                 Exercises
                             </NavLink>
                             <NavLink to="/conversations" className="nav-link">
+                                <ChatDots className="me-2" />
                                 Conversations
                             </NavLink>
                             <NavLink to="/exams" className="nav-link">
+                                <ClipboardCheck className="me-2" />
                                 Exams
                             </NavLink>
                         </Nav>

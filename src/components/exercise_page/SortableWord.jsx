a669ecdeeb337bc870d7b3b2785b300143056a45
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-09-01 10:49:08 +0700

    Better contrast for drag and drop activities

--- src/components/exercise_page/SortableWord.jsx
+++ src/components/exercise_page/SortableWord.jsx
@@ -1,10 +1,24 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { Button } from "react-bootstrap";
 import he from "he";
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 
 const SortableWord = ({ word, isCorrect, disabled, isOverlay }) => {
-    const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id: word.id });
+    const [itemWidth, setItemWidth] = useState(null);
+    // Ref for capturing the DOM element
+    const ref = React.useRef(null);
+
+    const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+        id: word.id,
+    });
+
+    useEffect(() => {
+        // Measure the width after the component mounts
+        if (ref.current) {
+            setItemWidth(ref.current.offsetWidth);
+        }
+    }, []);
 
     const style = {
         transform: CSS.Transform.toString(transform),
@@ -14,24 +28,24 @@ const SortableWord = ({ word, isCorrect, disabled, isOverlay }) => {
         userSelect: "none", // Prevent text selection
         WebkitUserDrag: "none", // Prevent dragging the element as a browser action (Safari)
         WebkitTouchCallout: "none", // Prevent showing callout on touching and dragging
-        ...(isDragging && !disabled && { opacity: 0.5 }), // Make the item semi-transparent while dragging
-        zIndex: isDragging ? 1000 : "auto", // Bring the dragging item to the front
-        boxShadow: isDragging ? "0 4px 8px rgba(0, 0, 0, 0.2)" : "none",
-        ...(!isOverlay && {
-            marginBottom: "1rem",
-        }),
+        width: isOverlay ? itemWidth : "", // Set the width to match with that of the current item
     };
 
-    const variant = isOverlay ? "light" : isCorrect === null ? "light" : isCorrect ? "success" : "danger";
+    const variant = isOverlay ? "secondary" : isCorrect === null ? "secondary" : isCorrect ? "success" : "danger";
 
     return (
         <Button
-            ref={setNodeRef}
+            ref={(node) => {
+                setNodeRef(node); // Pass the node to DnD kit
+                ref.current = node; // Capture the reference in our own ref
+            }}
             style={style}
             {...attributes}
             {...listeners}
             variant={variant}
-            className="mb-2 fw-bold"
+            className={`mb-2 fw-bold ${isDragging && !disabled ? "opacity-50" : ""} ${
+                isOverlay ? "z-2 shadow-sm" : ""
+            }`}
             disabled={disabled}>
             {he.decode(word.text)}
         </Button>

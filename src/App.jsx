8db8dedcc4638be836ca65077f39b4623a1ab2b8
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-09-03 08:12:42 +0700

    Prioritize loading main components
    Testing loading time part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import { Suspense, lazy } from "react";
 import { Container } from "react-bootstrap";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LoadingOverlay from "./components/general/LoadingOverlay";
 import { ThemeProvider } from "./utils/ThemeProvider";
 import ThemeSwitcher from "./utils/ThemeSwitcher";
-
-const Homepage = lazy(() => import("./components/Homepage"));
-const SoundList = lazy(() => import("./components/sound_page/SoundList"));
-const ConversationMenu = lazy(() => import("./components/conversation_page/ConversationMenu"));
-const ExamPage = lazy(() => import("./components/exam_page/ExamPage"));
-const ExcercisePage = lazy(() => import("./components/exercise_page/ExercisePage"));
+import Homepage from "./components/Homepage";
+import SoundList from ./components/sound_page/SoundList";
+import ConversationMenu from ./components/conversation_page/ConversationMenu";
+import ExamPage from ./components/exam_page/ExamPage";
+import ExcercisePage from ./components/exercise_page/ExercisePage";
 
 const App = () => {
     return (
         <Container className="p-4 mb-5">
             <Router>
-                <Suspense fallback={<LoadingOverlay />}>
-                    <Routes>
-                        <Route path="/" element={<Homepage />} />
-                        <Route path="/sounds" element={<SoundList />} />
-                        <Route path="/conversations" element={<ConversationMenu />} />
-                        <Route path="/exams" element={<ExamPage />} />
-                        <Route path="/exercises" element={<ExcercisePage />} />
-                    </Routes>
-                </Suspense>
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/sounds" element={<SoundList />} />
+                    <Route path="/conversations" element={<ConversationMenu />} />
+                    <Route path="/exams" element={<ExamPage />} />
+                    <Route path="/exercises" element={<ExcercisePage />} />
+                </Routes>
             </Router>
             <ThemeProvider>
                 <ThemeSwitcher />

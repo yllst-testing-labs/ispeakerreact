53aedbd684890eaab247142189c85c8a866acaec
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-09-03 08:26:42 +0700

    Split components excluding homepage
    Test loading time part 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import LoadingOverlay from "./components/general/LoadingOverlay";
 import { ThemeProvider } from "./utils/ThemeProvider";
 import ThemeSwitcher from "./utils/ThemeSwitcher";
 import Homepage from "./components/Homepage";
-import SoundList from "./components/sound_page/SoundList";
-import ConversationMenu from "./components/conversation_page/ConversationMenu";
-import ExamPage from "./components/exam_page/ExamPage";
-import ExcercisePage from "./components/exercise_page/ExercisePage";
+
+const SoundList = lazy(() => import("./components/sound_page/SoundList"));
+const ConversationMenu = lazy(() => import("./components/conversation_page/ConversationMenu"));
+const ExamPage = lazy(() => import("./components/exam_page/ExamPage"));
+const ExercisePage = lazy(() => import("./components/exercise_page/ExercisePage"));
 
 const App = () => {
     return (
@@ -15,10 +16,26 @@ const App = () => {
             <Router>
                 <Routes>
                     <Route path="/" element={<Homepage />} />
-                    <Route path="/sounds" element={<SoundList />} />
-                    <Route path="/conversations" element={<ConversationMenu />} />
-                    <Route path="/exams" element={<ExamPage />} />
-                    <Route path="/exercises" element={<ExcercisePage />} />
+                    <Route path="/sounds" element={
+                        <Suspense fallback={<LoadingOverlay />}>
+                            <SoundList />
+                        </Suspense>
+                    } />
+                    <Route path="/conversations" element={
+                        <Suspense fallback={<LoadingOverlay />}>
+                            <ConversationMenu />
+                        </Suspense>
+                    } />
+                    <Route path="/exams" element={
+                        <Suspense fallback={<LoadingOverlay />}>
+                            <ExamPage />
+                        </Suspense>
+                    } />
+                    <Route path="/exercises" element={
+                        <Suspense fallback={<LoadingOverlay />}>
+                            <ExercisePage />
+                        </Suspense>
+                    } />
                 </Routes>
             </Router>
             <ThemeProvider>

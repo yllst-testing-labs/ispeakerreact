903a5631213abc5d6a49e9ec97574467bdfcb7e5
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-12-15 13:36:54 +0700

    Update DictationQuiz.jsx

--- src/components/exercise_page/DictationQuiz.jsx
+++ src/components/exercise_page/DictationQuiz.jsx
@@ -5,6 +5,7 @@ import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
 import { IoInformationCircleOutline, IoVolumeHigh, IoVolumeHighOutline } from "react-icons/io5";
 import { LiaCheckCircle, LiaChevronCircleRightSolid, LiaTimesCircle } from "react-icons/lia";
 import useCountdownTimer from "../../utils/useCountdownTimer";
+import { sonnerErrorToast } from "../../utils/sonnerCustomToast";
 
 const DictationQuiz = ({ quiz, timer, onAnswer, onQuit, setTimeIsUp }) => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -20,7 +21,9 @@ const DictationQuiz = ({ quiz, timer, onAnswer, onQuit, setTimeIsUp }) => {
     const [isPlaying, setIsPlaying] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
 
-    const { formatTime, clearTimer, startTimer } = useCountdownTimer(timer, () => setTimeIsUp(true));
+    const { formatTime, clearTimer, startTimer } = useCountdownTimer(timer, () =>
+        setTimeIsUp(true)
+    );
 
     const audioRef = useRef(null);
 
@@ -142,7 +145,7 @@ const DictationQuiz = ({ quiz, timer, onAnswer, onQuit, setTimeIsUp }) => {
         audio.onerror = () => {
             setIsLoading(false);
             console.error("Audio error occurred. Unable to play the audio.");
-            alert("Unable to play audio due to a network issue. Please check your connection and reload the page.");
+            sonnerErrorToast(t("toast.audioPlayFailed"));
         };
 
         audio.onpause = () => {
@@ -156,7 +159,8 @@ const DictationQuiz = ({ quiz, timer, onAnswer, onQuit, setTimeIsUp }) => {
         const currentWords = shuffledQuiz[currentQuestionIndex]?.words || [];
 
         // Check if the current word has both `value` and `textbox`
-        const hasValueAndTextbox = currentWords.some((w) => w.value) && currentWords.some((w) => w.textbox);
+        const hasValueAndTextbox =
+            currentWords.some((w) => w.value) && currentWords.some((w) => w.textbox);
 
         return currentWords.map((word, index) => {
             if (word.value) {
@@ -173,8 +177,9 @@ const DictationQuiz = ({ quiz, timer, onAnswer, onQuit, setTimeIsUp }) => {
                 return (
                     <span
                         key={index}
-                        className={`inline-block my-2 ${hasValueAndTextbox ? "w-48" : "w-full lg:w-3/4"}`}>
-                        <label className="input input-bordered flex items-center md:gap-2 gap-0">
+                        className={`my-2 inline-block ${hasValueAndTextbox ? "w-48" : "w-full lg:w-3/4"}`}
+                    >
+                        <label className="input input-bordered flex items-center gap-0 md:gap-2">
                             <input
                                 type="text"
                                 value={answer}
@@ -185,8 +190,8 @@ const DictationQuiz = ({ quiz, timer, onAnswer, onQuit, setTimeIsUp }) => {
                                 autoComplete="off"
                                 spellCheck="false"
                                 disabled={isTextboxDisabled}
-                                className={`grow text-center${
-                                    hasValueAndTextbox ? " mx-2" : " mx-auto"
+                                className={`grow text-center ${
+                                    hasValueAndTextbox ? "mx-2" : "mx-auto"
                                 } text-black dark:text-slate-200`}
                                 style={hasValueAndTextbox ? { width: "40%" } : {}}
                             />
@@ -211,13 +216,13 @@ const DictationQuiz = ({ quiz, timer, onAnswer, onQuit, setTimeIsUp }) => {
     return (
         <>
             <div className="card-body">
-                <div className="font-semibold text-lg">
+                <div className="text-lg font-semibold">
                     {timer > 0 ? (
                         <div className="flex items-center">
                             <div className="flex-1 md:flex-none">
                                 {t("exercise_page.questionNo")} #{currentQuestionIndex + 1}
                             </div>
-                            <div className="flex justify-end ms-auto">
+                            <div className="ms-auto flex justify-end">
                                 {t("exercise_page.timer")} {formatTime()}
                             </div>
                         </div>
@@ -233,8 +238,9 @@ const DictationQuiz = ({ quiz, timer, onAnswer, onQuit, setTimeIsUp }) => {
                         type="button"
                         title={t("exercise_page.buttons.playAudioBtn")}
                         onClick={handleAudioPlay}
-                        className="btn btn-success btn-circle my-3"
-                        disabled={isLoading}>
+                        className="btn btn-circle btn-success my-3"
+                        disabled={isLoading}
+                    >
                         {isLoading ? (
                             <span className="loading loading-spinner loading-md"></span>
                         ) : isPlaying ? (
@@ -249,31 +255,38 @@ const DictationQuiz = ({ quiz, timer, onAnswer, onQuit, setTimeIsUp }) => {
                 </form>
                 {showValidation && validationVariant === "danger" && (
                     <div className="flex justify-center">
-                        <div role="alert" className="alert alert-info w-full lg:w-1/2 flex my-4 gap-1 md:gap-2">
-                            <IoInformationCircleOutline className="h-6 w-6 " />
+                        <div
+                            role="alert"
+                            className="alert alert-info my-4 flex w-full gap-1 md:gap-2 lg:w-1/2"
+                        >
+                            <IoInformationCircleOutline className="h-6 w-6" />
                             <div className="w-4/5 lg:w-auto">
                                 <h3>{t("exercise_page.result.correctAnswer")}</h3>
-                                <p className="font-bold italic text-xl">{validationMessage}</p>
+                                <p className="text-xl font-bold italic">{validationMessage}</p>
                             </div>
                         </div>
                     </div>
                 )}
                 <div className="card-actions justify-center">
-                    <div className="flex flex-wrap justify-center my-3 gap-2">
+                    <div className="my-3 flex flex-wrap justify-center gap-2">
                         <button
                             type="button"
                             className="btn btn-primary"
                             disabled={isSubmitButtonEnabled}
-                            onClick={handleSubmit}>
-                            <LiaCheckCircle className="h-6 w-6" /> {t("exercise_page.buttons.checkBtn")}
+                            onClick={handleSubmit}
+                        >
+                            <LiaCheckCircle className="h-6 w-6" />{" "}
+                            {t("exercise_page.buttons.checkBtn")}
                         </button>
                         {currentQuestionIndex < quiz.length - 1 && (
                             <button type="button" className="btn btn-accent" onClick={handleNext}>
-                                <LiaChevronCircleRightSolid className="h-6 w-6" /> {t("exercise_page.buttons.nextBtn")}
+                                <LiaChevronCircleRightSolid className="h-6 w-6" />{" "}
+                                {t("exercise_page.buttons.nextBtn")}
                             </button>
                         )}
                         <button type="button" className="btn btn-error" onClick={handleQuit}>
-                            <LiaTimesCircle className="h-6 w-6" /> {t("exercise_page.buttons.quitBtn")}
+                            <LiaTimesCircle className="h-6 w-6" />{" "}
+                            {t("exercise_page.buttons.quitBtn")}
                         </button>
                     </div>
                 </div>

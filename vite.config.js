753faf7d806f7ce191ff5a3f51e831b4de38cd2b
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-05-04 11:29:20 +0700

    disable chunking for electron for faster performance

--- vite.config.js
+++ vite.config.js
@@ -16,11 +16,19 @@ export default defineConfig(({ mode }) => {
         build: {
             rollupOptions: {
                 output: {
-                    manualChunks(id) {
-                        if (id.includes("node_modules")) {
-                            return id.toString().split("node_modules/")[1].split("/")[0].toString();
-                        }
-                    },
+                    ...(isElectron
+                        ? { inlineDynamicImports: true } // disables chunking for Electron
+                        : {
+                              manualChunks(id) {
+                                  if (id.includes("node_modules")) {
+                                      return id
+                                          .toString()
+                                          .split("node_modules/")[1]
+                                          .split("/")[0]
+                                          .toString();
+                                  }
+                              },
+                          }),
                 },
             },
             manifest: true,

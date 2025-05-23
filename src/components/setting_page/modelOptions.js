cba19ad6109769f6fc947c12ba7f43dd5222954d
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-05-23 13:36:12 +0700

    add new phoneme models from facebook

--- src/components/setting_page/modelOptions.js
+++ src/components/setting_page/modelOptions.js
@@ -1,13 +1,21 @@
 const modelOptions = [
     {
         value: "vitouphy/wav2vec2-xls-r-300m-timit-phoneme",
-        label: "vitouphy/wav2vec2-xls-r-300m-timit-phoneme",
+        description: "settingPage.pronunciationSettings.modelDescription1",
+        size: "1.26GB",
+    },
+    {
+        value: "facebook/wav2vec2-lv-60-espeak-cv-ft",
+        description: "settingPage.pronunciationSettings.modelDescription1",
+        size: "1.26GB",
+    },
+    {
+        value: "facebook/wav2vec2-xlsr-53-espeak-cv-ft",
         description: "settingPage.pronunciationSettings.modelDescription1",
         size: "1.26GB",
     },
     {
         value: "bookbot/wav2vec2-ljspeech-gruut",
-        label: "bookbot/wav2vec2-ljspeech-gruut",
         description: "settingPage.pronunciationSettings.modelDescription2",
         size: "378MB",
     },

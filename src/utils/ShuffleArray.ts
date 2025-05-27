11104ef5f41ead48ac81b476ca6fdf99a8820404
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-05-27 22:39:35 +0700

    update ShuffleArray

--- src/utils/ShuffleArray.ts
+++ src/utils/ShuffleArray.ts
@@ -1,7 +1,9 @@
-export const ShuffleArray = (array) => {
+const ShuffleArray = <T>(array: T[]): T[] => {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
 };
+
+export default ShuffleArray;

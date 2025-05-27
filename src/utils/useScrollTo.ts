43e34d4f7c8247728552caec68359d0e35741f2b
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-05-27 22:38:29 +0700

    update useScrollTo

--- src/utils/useScrollTo.ts
+++ src/utils/useScrollTo.ts
@@ -1,17 +1,20 @@
 import { useRef } from "react";
 
-export const useScrollTo = () => {
+const useScrollTo = () => {
     const ref = useRef(null);
     const padding = 300; // extra padding
 
     const scrollTo = (options = { behavior: "smooth" }) => {
         if (ref.current) {
             const element = ref.current;
-            const top = element.getBoundingClientRect().top + window.scrollY - padding;
+            const top =
+                (element as HTMLElement).getBoundingClientRect().top + window.scrollY - padding;
 
-            window.scrollTo({ top, behavior: options.behavior });
+            window.scrollTo({ top, behavior: options.behavior as ScrollBehavior });
         }
     };
 
     return { ref, scrollTo };
 };
+
+export default useScrollTo;

8d0dab02044837c31dc85addd7efaf1f365c540c
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-05-29 14:35:29 +0700

    update index.jsx to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../i18n";
-import App from "./App";
+import App from "./App.jsx";
 import "./styles/index.css";
 
-const root = createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = createRoot(rootElement);
 root.render(
     <StrictMode>
         <App />
